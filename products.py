e76593be7322eec8a91fc2027a06ee6bcaf55703
Albert Liao <pulieden@gmail.com>
2018-03-03 15:47:17 +0800

    判斷檔案是否存在

--- products.py
+++ products.py
@@ -4,18 +4,27 @@ Created on Sat Mar  3 13:02:53 2018
 
 @author: albert-w10
 """
-#讀取檔案
+
+
+import os #載入函式庫
 products = []
-with open('products.csv','r') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue #跳過本迴圈 不讀取 欄位名稱
-        name,price = line.strip().split(',') #去除\n 切割','  
-        products.append([name,price]) #加入清單
-print(products )
+if os.path.isfile('products.csv'): #判斷檔案是否存在
+    print('have some one file')
+    with open('products.csv','r') as f:#讀取檔案
+        for line in f:
+            if '商品,價格' in line:
+                continue #跳過本迴圈 不讀取 欄位名稱
+            name,price = line.strip().split(',') #去除\n 切割','  
+            products.append([name,price]) #加入清單
+    print(products)
+else:
+    print('no file.....')
 
 
 
+
+
+#使用者輸入
 #products = []
 while True:
     name = input('輸入商品名稱:')
