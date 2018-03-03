95c07c6b8fdf72c17fff4372a8c2aa9789042b56
Albert Liao <pulieden@gmail.com>
2018-03-03 14:18:38 +0800

    加入欄位名稱

--- products.py
+++ products.py
@@ -26,8 +26,8 @@ for p in products:
     
 #with open('products.txt','w') as f:
 with open('products.csv','w') as f:
-    for p in products:
-        
+    f.write('商品,價格\n') #加入欄位名稱
+    for p in products:        
         f.write(p[0] + ',' + str(p[1]) + '\n') #p[1] 改為str .casting
     
     
